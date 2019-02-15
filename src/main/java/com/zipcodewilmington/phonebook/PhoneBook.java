e21edb5775054b4b4ec18991aade71cdde3f4c81
Nirmala <nirmala.talari@gmail.com>
2019-02-14 19:07:06 -0500

    PhoneBookLab-PartSubmission

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,100 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private SortedMap<String, ArrayList<String>> phoneBook = new TreeMap<String, ArrayList<String>>();
+
+    //constructor
+    public  PhoneBook()
+    {}
+
+    public PhoneBook(String name, ArrayList phone) {
+        this.phoneBook.put(name,phone);
+    }
+
+    //adds an entry to the composite associate data type
+    public void add(String name, ArrayList phoneList) {
+        phoneBook.put(name, phoneList);
+    }
+
+    //Check if there is an entry in the phone book
+    public boolean hasEntry(String name)
+    {
+        boolean bool = false;
+        if (phoneBook.containsKey(name))
+        {bool = true;}
+        else
+        {bool = false;}
+
+        return bool;
+    }
+
+    //removes an entry to the composite associate data type
+    public void remove(String name, ArrayList phoneList) {
+        phoneBook.remove(name);
+    }
+
+    //returns a phone number for the respective input name
+    public ArrayList getPhoneNumbersList(String name) {
+        if (phoneBook.containsKey(name)) {
+            ArrayList<String> phoneList = phoneBook.get(name);
+            return phoneList;
+
+        } else {
+            ArrayList<Integer> phoneList = new ArrayList<Integer>();
+            return phoneList;
+        }
+    }
+
+    //print a human-readable list of all entries (names and phone numbers) in alphabetical order.
+    public void displayList() {
+        System.out.printf("The list of Phone Book Names and Phone Numbers:" + phoneBook.entrySet());
+    }
+
+    /*
+    public String lookup(String name) {
+        return string;
+    }
+
+     public String lookupNumbersFor(String name) {
+     return arrayList;
+    }
+
+    public String listNamesAndNumbers(String name) {
+     return string;
+    }
+
+     public String listNames(String name) {
+     return string;
+    }
+
+     public void add(String, string ...) {
+
+    }
+
+     public void removeRecord(String name) {
+
+    }
+
+    //returns a name for the respective input phoneNumber
+    public void reverseLookup(String phoneNumber) {
+
+
+    }
+
+    //get keys from values in the TreeMap
+    public static <T, E> Set<T> getKeysByValue(PhoneBook<T, E> map, E value) {
+
+    }
+
+*/
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,85 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    private SortedMap<String, ArrayList<String>> phoneBook = new TreeMap<String, ArrayList<String>>();
+
+    @Test
+    public void constructorTest() {
+        //Given
+        String expectedName = "John";
+        ArrayList<String> expectedPhone = new ArrayList<String>();
+        expectedPhone.add("212-343-5935");
+        //When
+        PhoneBook phoneBookObj = new PhoneBook(expectedName,expectedPhone);
+        boolean bool = phoneBookObj.hasEntry(expectedName);
+        //Then
+        Assert.assertTrue(bool);
+    }
+
+    @Test
+    public void addTest() {
+        //Given
+        String expectedName = "Sal";
+        ArrayList<String> expectedPhone = new ArrayList<String>();
+        expectedPhone.add("565-343-9685");
+        expectedPhone.add("292-324-9058");
+        //When
+        PhoneBook phoneBookObj = new PhoneBook();
+        phoneBookObj.add(expectedName, expectedPhone);
+        boolean bool = phoneBookObj.hasEntry(expectedName);
+        //Then
+        Assert.assertTrue(bool);
+    }
+
+    @Test
+    public void removeTest(){
+        //Given
+        String expectedName = "Ced";
+        ArrayList<String> expectedPhone = new ArrayList<String>();
+        expectedPhone.add("111-343-3546");
+        expectedPhone.add("911-324-9000");
+        String expectedName2 = "John";
+        ArrayList<String> expectedPhone2 = new ArrayList<String>();
+        expectedPhone2.add("212-343-5935");
+        PhoneBook phoneBookObj = new PhoneBook();
+        phoneBookObj.add(expectedName, expectedPhone);
+        phoneBookObj.add(expectedName2,expectedPhone2);
+        //When
+        phoneBookObj.remove(expectedName,expectedPhone);
+        phoneBookObj.remove(expectedName2,expectedPhone2);
+        boolean bool = phoneBookObj.hasEntry(expectedName);
+        boolean bool2 = phoneBookObj.hasEntry(expectedName);
+        //Then
+        Assert.assertFalse(bool);
+        Assert.assertFalse(bool2);
+    }
+
+    @Test
+    public void getPhoneNumbersListTest(){
+        //Given
+        String expectedName = "Ced";
+        Integer expectedPhoneList = 2;
+        ArrayList<String> expectedPhone = new ArrayList<String>();
+        expectedPhone.add("111-343-3546");
+        expectedPhone.add("911-324-9000");
+        PhoneBook phoneBookObj = new PhoneBook();
+        phoneBookObj.add(expectedName, expectedPhone);
+        //When
+        Integer actualPhoneList = (phoneBookObj.getPhoneNumbersList(expectedName)).size();
+        //Then
+        Assert.assertEquals(expectedPhoneList,actualPhoneList);
+    }
+
+
 }
